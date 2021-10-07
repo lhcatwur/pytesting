08dababbe2816c4f1cc470e38a049c7bad50dba4
lhcatwur <83064001+lhcatwur@users.noreply.github.com>
2021-10-07 12:22:22 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# File              : example.py
-# Date              : 07.10.2021
-# Last Modified Date: 07.10.2021
 def add(a, b):
     return a + b
 
@@ -11,5 +6,11 @@ def test_add():
     assert add(2, 3) == 5
     assert add('space', 'ship') == 'spaceship'
 
-def test_addFloat(tol=1e5):
-    assert abs(add(0.1, 0.2) -  0.3) <= tol     
+
+def subtract(a, b):
+    return a + b  # <--- fix this 
+
+
+# uncomment the following test
+#def test_subtract():
+#    assert subtract(2, 3) == -1
